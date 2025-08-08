75c358858caf99be763ce56cfa950c172e6abdda
yyyueyue <67182909+yyyueyue@users.noreply.github.com>
2025-08-09 01:23:10 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -112,7 +112,7 @@ const API_SITES = {
     },
     testSource: {
         api: 'https://www.example.com/api.php/provide/vod',
-        name: '空内容1测试源',
+        name: '空内容测试源1',
         adult: true
     },
     // 下面是一些成人内容的API源，默认隐藏，使用本项目浏览黄色内容违背项目初衷
